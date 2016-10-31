99a86e6ef83a975f3432e1dc333d454fba0a2f25
Peter Wells <pdwells08@gmail.com>
2016-10-31 12:06:20 -0400

    using loop for span nav elements.

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,5 @@
-function toggle_open(){
-  if($('div.mobile-nav').hasClass('closed') == true) {
-    $('div.mobile-nav').removeClass('closed');
-    $('div.mobile-nav').addClass('open');
-  } else {
-    $('div.mobile-nav').removeClass('open');
-    $('div.mobile-nav').addClass('closed');
-  }
-}
-
 function add_hamburger_onclick(){
-  //$('#hamburger').on('click',toggle_open);
-  $('#nav-icon4').click(function(){
+  $('#nav-icon').click(function(){
 		$(this).toggleClass('open');
     $('div.mobile-nav').toggleClass('open');
 	});

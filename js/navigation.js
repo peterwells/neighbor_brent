05fbba5e6c375d798a29780c754620b16b767a1d
Peter Wells <pdwells08@gmail.com>
2016-11-04 14:36:31 -0400

    added non mobile navigation area. now hides the non-mobile navigation when javascript is enabled and viewport is small enough

--- js/navigation.js
+++ js/navigation.js
@@ -5,7 +5,33 @@ function add_hamburger_onclick(){
 	});
 }
 
+function hide_non_mobile_nav(){
+  var viewportWidth = $(window).width();
+  var emSize = parseFloat($("body").css("font-size"));
+  var medViewportMin = emSize * 40.0625;
+  if (viewportWidth < medViewportMin){
+    $('nav.main ul').hide();
+  }
+}
+
+function show_non_mobile_nav(){
+  var viewportWidth = $(window).width();
+  var emSize = parseFloat($("body").css("font-size"));
+  var smallViewportMax = emSize * 40;
+  if (viewportWidth > smallViewportMax){
+    $('nav.main ul').show();
+  }
+}
+
+function add_check_for_size_change(){
+  $(window).on('resize', function(){
+    hide_non_mobile_nav();
+    show_non_mobile_nav();
+  })
+}
 
 $(window).on('load', function(){
+  hide_non_mobile_nav();
+  add_check_for_size_change();
   add_hamburger_onclick();
 })
